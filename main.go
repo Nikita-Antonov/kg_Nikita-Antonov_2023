93f60326422bba218fc3054d45c285a38f4091fc
Nikita Antonov <antonovenikita@gmail.com>
2021-04-28 18:08:43 -0700

    Updated Input + Added logic
    Changed the input to be passed through args. Added statements to translate the integer numbers to string.
    
    This program is dynamic, as the user can input as many values as they want.

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	//storedStr := []string{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
+	storedStr := []string{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
 
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	input := scanner.Text()
+	n := 1
+	for n < len(os.Args) {
+		inputValue := os.Args[n]
+		var numInInput int = len(inputValue)
+		n++
+		for x := 0; x < numInInput; x++ {
+			serchInt := ((inputValue[x]) - 48)
+			fmt.Print(storedStr[serchInt])
+		}
+		fmt.Print(" ")
+	}
 
-	fmt.Print(input)
 }
