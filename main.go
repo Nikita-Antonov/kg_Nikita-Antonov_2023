56f326f763ca9cf0ba3c49273e03f29b8992a680
Nikita Antonov <antonovenikita@gmail.com>
2021-04-28 21:25:11 -0400

    Optimization
    Optimized the code a little by condensing it, and added comments

--- main.go
+++ main.go
@@ -9,15 +9,13 @@ func main() {
 	storedStr := []string{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}
 
 	n := 1
-	for n < len(os.Args) {
-		inputValue := os.Args[n]
-		var numInInput int = len(inputValue)
+	for n < len(os.Args) { //Sets the max value of the loop to the length of the input
+		inputValue := os.Args[n] //Setting the inputs to a variable
 		n++
-		for x := 0; x < numInInput; x++ {
-			serchInt := ((inputValue[x]) - 48)
-			fmt.Print(storedStr[serchInt])
+		for x := 0; x < len(inputValue); x++ {
+			serchInt := ((inputValue[x]) - 48) //takes the ascii value of ghe inputed char, and normalizing it to an int
+			fmt.Print(storedStr[serchInt])     //Uses the int to get the word, coresponding with the input number
 		}
 		fmt.Print(",")
 	}
-
 }
